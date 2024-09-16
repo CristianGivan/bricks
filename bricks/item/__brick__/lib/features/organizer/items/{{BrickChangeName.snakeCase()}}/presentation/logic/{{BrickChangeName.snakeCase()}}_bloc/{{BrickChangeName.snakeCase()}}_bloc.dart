2a97cf491a8952e8b2c8b09cf9b49ba2445f07d7
Givan, Cristian <cristian.givan@continental-corporation.com>
2024-09-16 18:39:01 +0300

    add item

--- bricks/item/__brick__/lib/features/organizer/items/{{BrickChangeName.snakeCase()}}/presentation/logic/{{BrickChangeName.snakeCase()}}_bloc/{{BrickChangeName.snakeCase()}}_bloc.dart
+++ bricks/item/__brick__/lib/features/organizer/items/{{BrickChangeName.snakeCase()}}/presentation/logic/{{BrickChangeName.snakeCase()}}_bloc/{{BrickChangeName.snakeCase()}}_bloc.dart
@@ -1,4 +1,3 @@
-part of '{{BrickChangeName.snakeCase()}}_bloc.dart';
 
 class {{BrickChangeName.PascalCase()}}Bloc extends Bloc<{{BrickChangeName.PascalCase()}}BlocEvent, {{BrickChangeName.PascalCase()}}BlocState> {
 
