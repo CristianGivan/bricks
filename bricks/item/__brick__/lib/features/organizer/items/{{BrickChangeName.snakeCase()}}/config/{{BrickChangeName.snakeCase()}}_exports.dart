9f737c784cce7a82df6e22f37db54cefea160e9c
Givan, Cristian <cristian.givan@continental-corporation.com>
2024-09-17 07:57:23 +0300

    date domain

--- bricks/item/__brick__/lib/features/organizer/items/{{BrickChangeName.snakeCase()}}/config/{{BrickChangeName.snakeCase()}}_exports.dart
+++ bricks/item/__brick__/lib/features/organizer/items/{{BrickChangeName.snakeCase()}}/config/{{BrickChangeName.snakeCase()}}_exports.dart
@@ -1,8 +1,19 @@
 export '../data/models/{{BrickChangeName.snakeCase()}}_model_lazy_loaded.dart';
 export '../domain/entities/{{BrickChangeName.snakeCase()}}_entity.dart';
 export '../domain/entities/{{BrickChangeName.snakeCase()}}_entity_lazy_loaded.dart';
+export '../domain/repositories/{{BrickChangeName.snakeCase()}}_repository.dart';
 export '../presentation/logic/task_bloc/{{BrickChangeName.snakeCase()}}_bloc.dart';
 export '../presentation/screens/{{BrickChangeName.snakeCase()}}_add_screen.dart';
 export '../presentation/screens/{{BrickChangeName.snakeCase()}}_screen.dart';
 export '{{BrickChangeName.snakeCase()}}_init.dart';
 export '{{BrickChangeName.snakeCase()}}_routes.dart';
+export '{{BrickChangeName.snakeCase()}}_names.dart';
+export '../data/datasources/task_tag_link_local_data_source.dart';
+export '../data/models/task_tag_link_model.dart';
+export '../data/datasources/task_tag_link_local_data_source_drift.dart';
+import '../data/datasources/task_tag_link_local_data_source_drift.dart';
+
+export 'package:fo_fe/features/organizer/items/organizer_item/config/organizer_item_export.dart';
+
+export '../../../../../../core/db/drift/organizer_drift_db.dart';
+export 'package:equatable/equatable.dart';
--- bricks/item/__brick__/lib/features/organizer/items/{{BrickChangeName.snakeCase()}}/data/models/{{BrickChangeName.snakeCase()}}_model_lazy_loaded.dart
+++ bricks/item/__brick__/lib/features/organizer/items/{{BrickChangeName.snakeCase()}}/data/models/{{BrickChangeName.snakeCase()}}_model_lazy_loaded.dart
@@ -1,4 +1,4 @@
-
+import 'package:fo_fe/features/organizer/items/{{BrickChangeName.snakeCase()}}/domain/entities/{{BrickChangeName.snakeCase()}}_entity.dart';
 
 class {{BrickChangeName.pascalCase()}}ModelLazyLoaded extends {{BrickChangeName.pascalCase()}}Model {
  }
