d0fe07c57d7fa7694193271f9608105a03fab4d4
Givan, Cristian <cristian.givan@continental-corporation.com>
2024-09-26 07:16:46 +0300

    location

--- bricks/item/__brick__/{{BrickChangeName.snakeCase()}}/config/{{BrickChangeName.snakeCase()}}_exports.dart
+++ bricks/item/__brick__/{{BrickChangeName.snakeCase()}}/config/{{BrickChangeName.snakeCase()}}_exports.dart
@@ -1,3 +1,14 @@
+export 'package:equatable/equatable.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:fo_fe/core/db/drift/organizer_drift_db.dart';
+export 'package:fo_fe/features/organizer/items/organizer_item/config/organizer_item_export.dart';
+export 'package:get_it/get_it.dart';
+export 'package:go_router/go_router.dart';
+
+export '../data/datasources/{{BrickChangeName.snakeCase()}}_local_data_source.dart';
+export '../data/datasources/{{BrickChangeName.snakeCase()}}_local_data_source_drift.dart';
+export '../data/models/{{BrickChangeName.snakeCase()}}_model.dart';
 export '../data/models/{{BrickChangeName.snakeCase()}}_model_lazy_loaded.dart';
 export '../domain/entities/{{BrickChangeName.snakeCase()}}_entity.dart';
 export '../domain/entities/{{BrickChangeName.snakeCase()}}_entity_lazy_loaded.dart';
@@ -6,19 +17,5 @@ export '../presentation/logic/bloc/{{BrickChangeName.snakeCase()}}_bloc.dart';
 export '../presentation/screens/{{BrickChangeName.snakeCase()}}_add_screen.dart';
 export '../presentation/screens/{{BrickChangeName.snakeCase()}}_screen.dart';
 export '{{BrickChangeName.snakeCase()}}_init.dart';
-export '{{BrickChangeName.snakeCase()}}_routes.dart';
 export '{{BrickChangeName.snakeCase()}}_route_names.dart';
-export '../data/datasources/{{BrickChangeName.snakeCase()}}_local_data_source.dart';
-export '../data/models/{{BrickChangeName.snakeCase()}}_model.dart';
-export '../data/datasources/{{BrickChangeName.snakeCase()}}_local_data_source_drift.dart';
-export '../data/datasources/{{BrickChangeName.snakeCase()}}_local_data_source_drift.dart';
-
-export 'package:fo_fe/features/organizer/items/organizer_item/config/organizer_item_export.dart';
-export 'package:fo_fe/core/db/drift/organizer_drift_db.dart';
-
-export 'package:flutter_bloc/flutter_bloc.dart';
-export 'package:get_it/get_it.dart';
-export 'package:equatable/equatable.dart';
-export 'package:flutter/material.dart';
-export 'package:go_router/go_router.dart';
-
+export '{{BrickChangeName.snakeCase()}}_routes.dart';
